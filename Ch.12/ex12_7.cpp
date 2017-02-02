13f4e2c9308ad8e60aef7947fe54d7b7ab9fd847
Riemannstein <houyifeng1005@hotmail.com>
2017-02-02 15:52:54 -0500

    Adding exercise 12.7

--- Ch.12/ex12_7.cpp
+++ Ch.12/ex12_7.cpp
@@ -2,26 +2,50 @@
 #include <vector>
 #include <memory>
 
+
 using namespace std;
 
-auto make_with_shared_ptr(){
-	return make_shared<vector<int>>();
+shared_ptr<vector<int>> make_with_shared_ptr(){
+	return make_shared<vector<int>>(); 
 }
 
-auto populate(shared_ptr<vector<int>> vec){
-	for (int i; cout << "Please Enter:\n", cin >> i; vec->push_back(i));
-	return vec;
+auto populate(shared_ptr<vector<int>> pvec){
+	for(int i; cout << "Please enter an integer", cin >> *p.push_back(i);)
+	return pvec;
 }
 
-auto print(shared_ptr<vector<int>> vec) -> ostream& {
-	for (auto i : *vec)
-		cout << i << " ";
-	return cout;
+auto print(shared_ptr<vector<int>> pvec) -> ostream& {
+	for (auto i : *pvec)
+		cout << *i << " ";
+	return cout;	
 }
 
 int main(){
-	auto vec = populate(make_with_shared_ptr());
+	auto vec = populate(make_with_shared_ptr);
 	print(vec) << endl;
-	
 	return 0;
 }
+
+
+
+//auto make_with_shared_ptr(){
+	//return make_shared<vector<int>>();
+//}
+
+//auto populate(shared_ptr<vector<int>> vec){
+	//for (int i; cout << "Please Enter:\n", cin >> i; vec->push_back(i));
+	//return vec;
+//}
+
+//auto print(shared_ptr<vector<int>> vec) -> ostream& {
+	//for (auto i : *vec)
+		//cout << i << " ";
+	//return cout;
+//}
+
+//int main(){
+	//auto vec = populate(make_with_shared_ptr());
+	//print(vec) << endl;
+	
+	//return 0;
+//}
