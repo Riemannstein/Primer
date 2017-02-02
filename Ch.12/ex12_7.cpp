caddec698667878431ab502cb33f5b7e3acbff82
Riemannstein <houyifeng1005@hotmail.com>
2017-02-02 17:25:52 -0500

    Add testing shared_ptr

--- Ch.12/ex12_7.cpp
+++ Ch.12/ex12_7.cpp
@@ -10,19 +10,24 @@ shared_ptr<vector<int>> make_with_shared_ptr(){
 }
 
 auto populate(shared_ptr<vector<int>> pvec){
-	for(int i; cout << "Please enter an integer", cin >> *p.push_back(i);)
+	for(int i; cout << "Please enter an integer:\n", cin >> i;
+		pvec->push_back(i));
 	return pvec;
 }
 
+// Never seen a definition with "->" after the function name
+// Trailing return?
 auto print(shared_ptr<vector<int>> pvec) -> ostream& {
 	for (auto i : *pvec)
-		cout << *i << " ";
+		cout << i << " ";
 	return cout;	
 }
 
 int main(){
-	auto vec = populate(make_with_shared_ptr);
+	auto vec = populate(make_with_shared_ptr());
 	print(vec) << endl;
+	//for(auto i : *vec)
+		//cout << i << endl;
 	return 0;
 }
 
